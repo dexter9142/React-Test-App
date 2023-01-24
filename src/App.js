2fdc9d30cd9f325536c26581ac749a2bc5797fff
dexter9142 <dax0068@gmail.com>
2023-01-25 01:43:55 +0200

    React Test Searchbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "./List";
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {BrowserRouter, NavLink} from "react-router-dom";
 
-export default App;
+export default function App() {
+
+return (
+    <BrowserRouter>
+        <Wrapper>
+        <Holder>
+            <List />
+        </Holder>
+        </Wrapper>
+    </BrowserRouter>
+
+)
+    }
+
+const Wrapper = styled.section`
+  padding: 4em;
+  min-height: 100vh;
+  background: lightblue;
+`;
+
+const Holder = styled.div`
+  display: flex;
+  justify-content: center;
+margin: auto;
+`;
